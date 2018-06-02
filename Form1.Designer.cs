b5ca39734b3f2a69fbc148ea674019a59eec8589
Yousef Moh7 <yousefmoh7@gmail.com>
2018-06-02 23:58:47 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace FBNews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(270, 89);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Keyword";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(181, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 32);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Page Id";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1191, 158);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 67);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Add Page";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(312, 156);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(278, 67);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(312, 70);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(307, 63);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(672, 66);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(198, 67);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Add Keyword";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(856, 158);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(241, 67);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(655, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Page Name";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1506, 783);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using FBNews.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FBNews
+{
+    public partial class Form1 : Form
+    {
+        string token = "EAACEdEose0cBAIZAEPvlp02uj2i1Fr9AhbVk9lBJgb1p82lfZC8eQwFs4wBbvkZCUFuoHbsn2fod3zcy7vVdjsUQbhMYokTHl4XWXRPYPM3DqP4iWhSgAjQWX52GHelqhZB9IBOYgoOck7VJLITLFKhyw2KmBiw8CfaSOnodnKN950tqeugymUViDvCw2ooZD";
+        string Url = "https://graph.facebook.com/155869377766434/posts?limit=100&access_token=";
+        List<page> pages;
+        FBNewsDbEntities fbEntities;
+        string[] keywords;
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fbEntities = new FBNewsDbEntities();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Url = Url + token;
+            pages = fbEntities.pages.ToList();
+            keywords = fbEntities.keywords.Select(c => c.keywords).ToArray();
+
+
+            foreach (page page in pages)
+            {
+
+
+
+                path = "https://graph.facebook.com/" + page.pagefbid + "/posts?limit=100&access_token=" + token;
+                var clientRepsonse = "";
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        clientRepsonse = client.DownloadString(path);
+                    }
+                    int count = 0;
+
+                    dynamic deserialize_post = JsonConvert.DeserializeObject(clientRepsonse);
+                    if (deserialize_post.data != null)
+                    {
+
+                        // check if posts exist
+                        var listresponse = deserialize_post.data;
+                        DateTime lastCheckDate = listresponse[0].created_time;
+
+                        for (int i = 0; i < listresponse.Count; i++)
+                        {
+                            count = listresponse.Count;
+                            string message = listresponse[i].message;
+                            DateTime createdate = listresponse[i].created_time;
+
+
+
+                            if (!string.IsNullOrEmpty(message))
+                            {
+                                foreach (string keyword in keywords)
+                                {
+                                    if (message.ToLower().Contains(keyword.ToLower()) && createdate > page.lastcheckdate)
+                                    {
+                                        FBNew fb = new FBNew();
+                                        fb.created_at = createdate;
+                                        fb.message = message;
+                                        fb.pagename = page.pagename;
+                                        fbEntities.FBNews.Add(fb);
+                                        fbEntities.SaveChanges();
+
+                                    }
+                                }
+
+                            }
+                        }
+                        page.lastcheckdate = lastCheckDate;
+                        fbEntities.SaveChanges();
+                    }
+                   // MessageBox.Show(count + "");
+
+                    // Do further processing.
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + "");
+                }
+            }
+
+            MessageBox.Show("Done ! Comeback tomworro ");
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string key = "";
+            keyword keyword = new keyword();
+            if (textBox4.Text == null)
+            {
+                MessageBox.Show("Please Empty values not allowed");
+                return;
+            }
+
+            key = textBox4.Text;
+            keyword.keywords = key;
+
+            if (fbEntities.keywords.Any(c => c.keywords.Trim().ToLower() == key.ToLower().Trim()))
+            {
+                MessageBox.Show("This Keyword already exist !");
+                return;
+
+            }
+
+            fbEntities.keywords.Add(keyword);
+            if (fbEntities.SaveChanges() == 1)
+                MessageBox.Show("Added Sucessfully ");
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string pageid = "";
+            string pagename = "";
+
+            page page = new page();
+
+            if (string.IsNullOrWhiteSpace(textBox3.Text) ||string.IsNullOrWhiteSpace(textBox5.Text))
+            {
+                MessageBox.Show("Please Empty values not allowed");
+                return;
+            }
+            if (fbEntities.pages.Any(c => c.pagefbid.Trim().ToLower() == textBox3.Text.ToLower().Trim()))
+            {
+                MessageBox.Show("This Page already exist !");
+                return;
+
+            }
+            pageid = textBox3.Text;
+            pagename = textBox5.Text;
+            page.pagefbid = pageid;
+            page.pagename=pagename;
+
+
+            fbEntities.pages.Add(page);
+            if (fbEntities.SaveChanges() == 1)
+                MessageBox.Show("Added Sucessfully ");
+
+
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
